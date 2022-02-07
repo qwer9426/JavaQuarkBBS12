c5e9ba78cc14efa7a3e9a1163373dd09f898b054
itcast <itcast@it>
2022-02-07 17:27:19 +0800

    Changes

--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -6,7 +6,7 @@ package com.quark.rest.utils;
  */
 public class Constants {
     //Upload常量
-    public static final String UPLOAD_PATH = "/opt/tmp";
+    public static final String UPLOAD_PATH = "/opt/tmp/";
     public static final String STATIC_URL ="http://www.yzzy.top/images/upload/";
 
 }
